675ad42c7c553e7034f67fa2989bdaeb29a47924
Miles Cruz <cruz.r.miles@gmail.com>
2019-05-10 20:43:44 -0500

    Addition of photos and updates to the handlers and the addition of new components

--- src/Output/Conclusion.js
+++ src/Output/Conclusion.js
@@ -4,23 +4,11 @@ import React from 'react';
 const conclusion = (props) => {
     return (
             <div className="conclusion" onchange={props.changed}>
-            <h1>FastChart</h1>
-            <ul>
-                <li>Miles Cruz</li>
-                <li>Spring 2019</li>
-            </ul>
-                    <p>
-
-            
-            This application was to be tentatively titled "HealthTracker". Later in development it was changed to "FastChart" which reflected it's new aim towards front-facing healthcare professionals.
-            This application will be used to download the user's FHIR records from supported healthcare institutions. 
-            The system then updates the records in the background on a scheduled basis. 
-            The user will have to consent to give out clinical data however, test data will be used in preliminary testing. 
-            With more updates, the possibility of integrating an iWatch app to compare real-time data pulled from the FHIR records could be used to better track a person's state based on their medical history. 
-            The application will be primarily developed using the Xcode IDE on the Mac OSX operating system. 
-            This application will be an iOS app utilizing the swift programming language as its source. 
-            The structure of the presentation will be orally based, with a simulation of the app running to show the application when in use on the iOS framework. 
-            I will put together a short video showing a short synopsis of how the app was made as well.
+        <p>
+            I believe that with more research on the functionality given by the HL7® FHIR® framework,
+            my goal of being able to change EHR data in real-time may actually be able to be accomplished.
+            Perhaps however, this kind of app would be better over a browser especially because of the lightweight nature of applications hosted
+            within browsers.
         </p>
 
             </div>
--- src/Output/DesignImplementation.js
+++ src/Output/DesignImplementation.js
@@ -1,28 +1,28 @@
 import React from 'react';
+import './Output.css';
+import connectview from './ConnectView.png';
+import endpointsview from './EndpointsView.png';
+import patientsingleview from './PatientSingleView.png';
+import patientview from './PatientsView.png';
+import patientviewprop from './PatientViewProp.png';
 
 
 
 const design = (props) => {
     return (
             <div className="design" onchange={props.changed}>
-            <h1>FastChart</h1>
-            <ul>
-                <li>Miles Cruz</li>
-                <li>Spring 2019</li>
-            </ul>
-                    <p>
-
-            
-            This application was to be tentatively titled "HealthTracker". Later in development it was changed to "FastChart" which reflected it's new aim towards front-facing healthcare professionals.
-            This application will be used to download the user's FHIR records from supported healthcare institutions. 
-            The system then updates the records in the background on a scheduled basis. 
-            The user will have to consent to give out clinical data however, test data will be used in preliminary testing. 
-            With more updates, the possibility of integrating an iWatch app to compare real-time data pulled from the FHIR records could be used to better track a person's state based on their medical history. 
-            The application will be primarily developed using the Xcode IDE on the Mac OSX operating system. 
-            This application will be an iOS app utilizing the swift programming language as its source. 
-            The structure of the presentation will be orally based, with a simulation of the app running to show the application when in use on the iOS framework. 
-            I will put together a short video showing a short synopsis of how the app was made as well.
-        </p>
+                <img src={endpointsview} width={200} height={400} mode='fit'/>
+                <img src={connectview} width={200} height={400} mode='fit'/>
+                <img src={patientview} width={200} height={400} mode='fit'/>
+                <img src={patientsingleview} width={200} height={400} mode='fit'/>
+                <img src={patientviewprop} width={200} height={400} mode='fit'/>
+                <p>
+                    This app was made in xcode in the mojave macosx environment.
+                    The source used for the application is the Swift programming language created by Apple.
+                    I also used online creation tools
+                    <a className="App-link" href="https://appicon.co/" target="_blank" rel="noopener noreferrer">appicon.co</a>
+                    to create my app.
+                </p>
 
             </div>
 
